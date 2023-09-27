ac2b98f527357fa79c20a39a7b0cf890cb0ceec5
Ana <mihalceanuana@gmail.com>
2023-09-27 18:11:37 +0200

    Cleanup poms and devspace.yaml

--- task-buddy/src/main/java/org/acme/example/TodoException.java
+++ task-buddy/src/main/java/org/acme/example/TodoException.java
@@ -5,10 +5,6 @@ public class TodoException extends RuntimeException {
 	public TodoException() {
 	}
 
-	public TodoException(Throwable cause) {
-		super(cause);
-	}
-
 	public TodoException(String message) {
 		super(message);
 	}
